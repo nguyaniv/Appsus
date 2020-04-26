61443c6416f6dec4ed09e4f20a6959e453c36d80
nguyaniv <nguyaniv@gmail.com>
2020-04-26 14:59:40 +0300

    add components

---
+++ cmps/NotePreview.jsx
@@ -0,0 +1,45 @@
+export default function NotePreview(props) {
+    const { note } = props
+
+
+    return (
+        <article className="note-preview">
+
+            {note && note.type === 'NoteText' &&
+                <section>
+                    <h2>{note.type}  </h2>
+                    <p>{note.info.txt}</p>
+
+                </section>
+            }
+
+
+            {note && note.type === 'NoteImg' &&
+
+
+                <section>
+                    {console.log(note.info.url)}
+                    <h2>{note.type}  </h2>
+                    <img src={note.info.url} alt="" />
+
+                </section>
+            }
+
+
+            {note && note.type === 'NoteTodos' &&
+                <section>
+                    <h2>{note.type}</h2>
+
+                    {note.info.todos.map((todo) => {
+                 return       <div key={todo.id}>
+                            {console.log(todo.txt)}
+                            <p>{todo.txt}</p>
+                        </div>
+
+                    })}
+
+                </section>
+            }
+        </article>
+    )
+}
